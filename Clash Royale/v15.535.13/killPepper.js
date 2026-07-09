d961cb3a607fe220f8bfc506e9e8822e22634d4c
Cartyrty <zvyqvpbwtt@privaterelay.appleid.com>
2026-07-09 11:21:14 -0400

    better way of handling nonce

--- Clash Royale/v15.535.13/killPepper.js
+++ Clash Royale/v15.535.13/killPepper.js
@@ -20,7 +20,7 @@ class CryptoKiller {
 
     static killEncryption() {
         Interceptor.attach(libg.add(0x1237590), { // auth function
-            onEnter(args) {infunc = true},onLeave() {infunc = false}})
+            onEnter(args) {infunc = true;},onLeave() {infunc = false}})
         Interceptor.replace(libg.add(0x1387FA4), new NativeCallback((a1, a2, a3, a4, a5, a6) => { // encrypter function
                 Memory.copy(a3, a1, Number(a2)) // write unencrypted payload to encrypted payload
                 console.log("[+] Skipped encryption");
@@ -29,19 +29,9 @@ class CryptoKiller {
         Interceptor.attach(libg.add(0x1213450), { // encrypted payload setter function
             onEnter(args) {
                 if (!infunc) return; // so we can get specifically the call from the auth function
-                const malloc = new NativeFunction(Module.findGlobalExportByName('malloc'),'pointer',['int'])
-                const free   = new NativeFunction(Module.findGlobalExportByName('free'),'void',['pointer'])
-
-                let noncelength = 80 // after encryption (which we just skipped) the auth function will append Client Secret Key and other crypto related stuff, we must next cut it off so the server receives it as normal
-                let size = Number(args[2]) - noncelength
-
-                let final = malloc(size) // realloc new pointer
-                Memory.copy(final, args[1].add(noncelength), size) // shift pointer to cut off nonce (basically .slice in js) and copy it to final
-                
-                free(args[1]) // free orginal pointer since we aren't using it anymore  
-                args[1] = final; // set it to final buffer
-                args[2] = ptr(size); // resize size
-
+                // After encryption (which we just skipped) the auth function will append Client Secret Key and other crypto related stuff to the header of the paylaod, we must next cut it off so the server receive it as normal
+                args[1] = args[1].add(80) // move the payload pointer to cut off crypto header
+                args[2] = args[2].sub(80) // resize the payload length
                 // console.log(args[1].readByteArray(Number(args[2])))
             }
         });
@@ -62,11 +52,11 @@ class CryptoKiller {
 }
 
 class Main {
-    static ip = Memory.allocUtf8String("192.168.1.107")
+    static ip = Memory.allocUtf8String("192.168.56.1")
     static port = Memory.allocUtf8String("9339")
     static main() {
         this.redirect()
-        console.log(`Initlizing v15 crypto kill...`) 
+        console.log(`Initializing v15 crypto kill...`) 
         CryptoKiller.init()
     }
     static redirect() {
